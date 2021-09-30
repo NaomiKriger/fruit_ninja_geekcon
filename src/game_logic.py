5ed33bb1f4bf6403adbaa81c22d030180edd8a68
HillaShx <hilla.sh@gmail.com>
2021-10-01 00:37:16 +0300

    game is over when player missed 5 hits

--- src/game_logic.py
+++ src/game_logic.py
@@ -19,6 +19,7 @@ CURSOR_COLOR = (209, 238, 239)
 clock = pygame.time.Clock()
 g = 1
 score = 0
+missed_count = 5
 FPS = 13
 FRUITS = ['watermelon', 'orange', 'apple']
 
@@ -30,6 +31,7 @@ font = pygame.font.Font(
     32,
 )
 score_text = font.render(str(score), True, BLACK, WHITE)
+missed_text = font.render(str(missed_count), True, (255, 0, 0), WHITE)
 
 
 def generate_random_fruit(fruit: str) -> None:
@@ -63,41 +65,58 @@ pygame.display.update()
 
 
 def game_loop():
-    global score_text, score
-    gameDisplay.fill(WHITE)
-    gameDisplay.blit(score_text, (0, 0))
-    for key, value in data.items():
-        if value['throw']:
-            value['x'] = value['x'] + value['speed_x']
-            value['y'] = value['y'] + value['speed_y']
-            value['speed_y'] += (g * value['t'])
-            value['t'] += 1
+    global score_text, score, missed_count, missed_text
 
-            if value['y'] <= 800:
-                gameDisplay.blit(value['img'], (value['x'], value['y']))
+    keep_going = True
+    while keep_going:
+        gameDisplay.fill(WHITE)
+        gameDisplay.blit(score_text, (0, 0))
+        gameDisplay.blit(missed_text, (0, 50))
+        for key, value in data.items():
+            if value['throw']:
+                value['x'] = value['x'] + value['speed_x']
+                value['y'] = value['y'] + value['speed_y']
+                value['speed_y'] += (g * value['t'])
+                value['t'] += 1
+
+                if value['x'] < 0 or value['x'] > WIDTH:
+                    generate_random_fruit(key)
+                if value['y'] <= 800:
+                    gameDisplay.blit(value['img'], (value['x'], value['y']))
+                else:
+                    if not value['hit']:
+                        missed_count -= 1
+                    if missed_count == 0:
+                        pygame.display.update()
+                        keep_going = False
+                    missed_text = font.render(str(missed_count), True, (255, 0, 0), WHITE)
+                    generate_random_fruit(key)
+
+                current_position = pygame.mouse.get_pos()
+                paint_cursor(gameDisplay, current_position)
+
+                if is_hit(current_position, value):
+                    path = MEDIA_PATH / 'sprites' / ('half_' + key + '.png')
+                    value['img'] = pygame.image.load(path)
+                    value['speed_x'] += 10
+                    score += 1
+                    score_text = font.render(str(score), True, BLACK, WHITE)
+                    value['hit'] = True
+
             else:
                 generate_random_fruit(key)
 
-            current_position = pygame.mouse.get_pos()
-            paint_cursor(gameDisplay, current_position)
+        pygame.display.update()
+        clock.tick(FPS)
 
-            if is_hit(current_position, value):
-                path = MEDIA_PATH / 'sprites' / ('half_' + key + '.png')
-                value['img'] = pygame.image.load(path)
-                value['speed_x'] += 10
-                score += 1
-                score_text = font.render(str(score), True, BLACK, WHITE)
-                value['hit'] = True
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-        else:
-            generate_random_fruit(key)
-
-    pygame.display.update()
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+    game_over_text = font.render('GAME OVER', True, BLACK, WHITE)
+    game_over_x = gameDisplay.get_width() // 2 - game_over_text.get_width() // 2
+    game_over_y = gameDisplay.get_height() // 2 - game_over_text.get_height() // 2
+    gameDisplay.blit(game_over_text, (game_over_x, game_over_y))
 
 
 def is_hit(current_position, value):
